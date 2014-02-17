f7f05d346fc34ac5a658fc27553ac0f4f2f5bbdc
Dennis Bartlett <bartlett.dc.1@gmail.com>
2014-02-16 19:24:15 -0600

    Moved Site Plans into a folder; added concurix for montioring

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-var Api = require('./bin/api'),
-	Web = require('./bin/web');
+// Setup Config
+var config = {}
+config.monitor = true;
 
-Api.start();
-Web.start();
+// Require Monitoring
+if (config.monitor = true) {
+	var cx = require('concurix-monitor')({
+	   accountKey: "b0e46fbeeb62320212c4aa42d0c914b8"
+	});
+	cx.start();
+}
+
+// Require App
+var Api      = require('./bin/api');
+var Git      = require('./bin/git');
+	// Web = require('./bin/web');
+
+// Api.start();
+// Web.start();
